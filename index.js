596315ec1b23b5201e9e450cc4b1edf7f111591c
TimSchulzRC <TimSchulzRC@github.com>
2020-12-16 17:20:00 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var drumButtons = document.querySelectorAll(".drum");
+
+for (i in drumButtons) {
+  document.querySelectorAll(".drum")[i].onclick = function () {
+    switch (this.innerHTML) {
+      case "w":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+      case "a":
+        var kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+        break;
+      case "s":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+      case "d":
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+        break;
+      case "j":
+        var audio = new Audio("sounds/tom-2.mp3");
+        audio.play();
+        break;
+      case "k":
+        var audio = new Audio("sounds/tom-3.mp3");
+        audio.play();
+        break;
+      case "l":
+        var audio = new Audio("sounds/tom-4.mp3");
+        audio.play();
+        break;
+      default:
+        console.log(this.innerHTML);
+
+      //   this.style.color = "white";
+    }
+  };
+}
